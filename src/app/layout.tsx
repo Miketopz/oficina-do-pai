1d4bbcc00c0c28bc7b92909afdd7f3bd1da08916
Alvaro <alvaro.faquim@discente.ufg.br>
2026-01-14 15:48:19 -0300

    chore: bump version to v4.1 for deployment check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
                 <div className="min-h-screen flex flex-col">
                     {children}
                     <Toaster richColors position="top-center" />
+                    <footer className="text-center text-xs text-gray-500 py-4 opacity-50">
+                        V4.1 - SISTEMA ATUALIZADO (14/01)
+                    </footer>
                 </div>
             </body>
         </html>
